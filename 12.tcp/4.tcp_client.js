834fbc98117e05b9315d3e4dae8f7e42cd5c3f66
zhangrenyang-t510 <zhang_renyang@>
2015-09-12 17:45:16 +0800

    udp

--- 12.tcp/4.tcp_client.js
+++ 12.tcp/4.tcp_client.js
@@ -3,7 +3,7 @@ var util = require('util');
 
 var socket = new net.Socket();
 socket.setEncoding('utf8');
-socket.connect(9999,'192.168.1.107',function(){
+socket.connect(8081,'192.168.1.163',function(){
     console.log(socket.remoteAddress,socket.remotePort,socket.localAddress,socket.localPort);
     socket.write('hello');
 });
