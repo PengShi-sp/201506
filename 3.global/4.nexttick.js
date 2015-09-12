2374b348190d10ccfe68c8da11b0355b71dafe42
zhangrenyang-t510 <zhang_renyang@>
2015-09-12 10:57:03 +0800

    global

---
+++ 3.global/4.nexttick.js
@@ -0,0 +1,23 @@
+/**
+ *
+ */
+function say(){
+   var sum = 1;
+    for(var i=1;i<=100;i++){
+        sum = sum*i;
+    }
+    console.log(sum);
+}
+//say();
+//setTimeout(say,300);
+function say1(){
+    console.log('say1');
+}
+function say2(){
+    console.log('say2');
+}
+setImmediate(say2);
+process.nextTick(say1);
+console.log('welcome1');
+console.log('welcome2');
+// hello  welcome
